ab7d709fb6a820f643d0c0ccbed42d660e45eba4
m4967893 <m18519743425@163.com>
2017-05-10 21:34:54 +0800

    添加登录注册

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
