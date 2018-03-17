cb209c0e10ac2668aa5f386d04a4931e12d32ae9
wangchuang <wangchuang@tigerwit.com>
2018-03-18 03:55:05 +0800

    修改页面

--- blog/resources/views/departure/apply.blade.php
+++ blog/resources/views/departure/apply.blade.php
@@ -13,24 +13,29 @@
         {{--<div class="row margin-top-10">--}}
             <div class="portlet light col-md-9">
                 {{--<div class="portlet light">--}}
-                <div class="col-md-12">
+                <div class="col-md-12" style="text-align:center;">
                     <h1>离职申请</h1>
                 </div>
-                <div class="col-md-12" style="height: 20px;"></div>
+                <div class="col-md-12" style="height: 30px;"></div>
                 <div class="col-md-12">
                     <form class="form-inline" role="form" action="/demand/index" method="get">
                         <div class="col-md-12">
                             <h4>岗位信息</h4>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">事业群:</label>
-                                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                            </div>
+                                <label for="exampleInputPassword1">事业群</label>
+                                <select class="form-control add_center_id">
+                                    <option>请选择</option>
+                                    <option value="">移动新闻广告事业群</option>
+                                    <option value="">今日头条广告事业群</option>
+                                    <option value="">品牌中心</option>
+                                </select>
+                            </div>&nbsp;&nbsp;&nbsp;
                             <div class="form-group">
-                                <label for="exampleInputPassword1">事业部:</label>
+                                <label for="exampleInputPassword1">事业部</label>
                                 <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                            </div>
+                            </div>&nbsp;&nbsp;&nbsp;
                             <div class="form-group">
-                                <label for="exampleInputPassword1">岗位:</label>
+                                <label for="exampleInputPassword1">岗位</label>
                                 <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
                         </div>
@@ -38,11 +43,11 @@
                         <div class="col-md-12">
                             <h4>员工信息</h4>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">离职人员姓名:</label>
+                                <label for="exampleInputPassword1">离职人员姓名</label>
                                 <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                            </div>
+                            </div>&nbsp;&nbsp;&nbsp;
                             <div class="form-group">
-                                <label for="exampleInputPassword1">最后工作日期:</label>
+                                <label for="exampleInputPassword1">最后工作日期</label>
                                 <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
                             <p></p>
