c5c10e8a5aeb150ce2023fd423becb45c3d2b449
wangchuang <wangchuang@tigerwit.com>
2018-03-18 02:48:22 +0800

    修改页面

--- blog/resources/views/departure/apply.blade.php
+++ blog/resources/views/departure/apply.blade.php
@@ -13,9 +13,9 @@
         <div class="row margin-top-10">
             {{--<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">--}}
             {{---------------------------------------------------------------------------------------------------------- }}               --}}
-            <div class="col-md-12">
+            <div class="col-md-9">
             <div class="portlet light">
-
+                <h1>离职申请</h1>
             </div>
         </div>
     </div>
